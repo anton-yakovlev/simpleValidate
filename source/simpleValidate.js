9340feea9928a1748d7d35a6cc76d5c0beb3c4e8
Anton Yakovlev <yakovlev.business@gmail.com>
2015-12-17 13:00:20 +0200

    Structure fix

--- source/simpleValidate.js
+++ source/simpleValidate.js
@@ -8,166 +8,162 @@
         emptyMessage: 'Заполните поле'
     };
 
-    $.fn.validate = function (options) {
-
-        var settings = $.extend({}, defaults, options);
-
-        this.init = function () {
-            console.log('[ Start Validate Module ... ]');
-
-            var form = $(this),
-                //formClass = '.' + form.attr('class'),
-                formInputs = form.find('input, textarea'),
-                errors = [],
-                serverUrl = form.attr('data-url');
-
-            /*console.log(form);
-            console.log(formInputs);
-            console.log(formClass);*/
-
-            //Start module when submitted
-            $(document).on('submit', form, function (e) {
-                e.preventDefault();
-
-                console.log('[ Submit pushed ... ]');
-
-                _checkEvery(formInputs);
-                _addListeners(formInputs);
-                _sendData();
-            });
-
-            function _addListeners(selectors) {
-                if (selectors) {
-
-                    for (var i = 0; i < selectors.length; i++) {
-                        var currentItem = $(selectors[i]);
-
-                        (function (ci) {
-                            $(document).on('keyup', currentItem, function () {
-                                _checkCurrent(ci);
-                            });
-                        })(currentItem);
-
-                    }
-
-                }
-            }
-
-            function _checkCurrent(selector) {
-                var valid = !_isEmpty(selector.val());
-                valid ? _doValid(selector) : _doInvalid(selector);
-            }
-
-            function _checkEvery(selectors) {
-                if (selectors) {
-                    errors = [];
-
-                    for (var i = 0; i < selectors.length; i++) {
-                        var currentInput = $(selectors[i]);
-                        _checkCurrent(currentInput);
-                    }
-                }
-            }
-
-            function _isEmpty(value) {
-                return !Boolean(value);
-            }
-
-            function _doValid(selector) {
-                var itemId = selector.attr('id'),
-                    errorLabelId = selector.attr('name') + '-error',
-                    index;
-
-                selector.removeClass(settings.errorInputClass);
-                index = $.inArray(itemId, errors);
-                errors.splice(index, 1);
-                $('#' + errorLabelId).remove();
-            }
-
-            function _doInvalid(selector) {
-                var itemId = selector.attr('id'),
-                    errorLabelId = selector.attr('name') + '-error',
-                    errorLabelLeft = selector.attr('data-error-left'),
-                    errorLabelLeftClass = selector.attr('data-error-left-class'),
-                    errorLabelMessage = selector.attr('data-empty-error'),
-                    errorLabelClasses;
-
-                selector.addClass(settings.errorInputClass);
-                errors.push(itemId);
-
-                if ($('#' + errorLabelId).length === 0) {
-                    $('<label>', {
-                        id: errorLabelId,
-                        text: function () {
-                            return errorLabelMessage ? errorLabelMessage : settings.emptyMessage;
-                        },
-                        'for': itemId,
-                        'class': (function () {
-                            if (errorLabelLeft) {
-                                errorLabelClasses = settings.errorLabelClass + ' ' + (errorLabelLeftClass ? errorLabelLeftClass : settings.errorLabelLeftClass);
-                            } else {
-                                errorLabelClasses = settings.errorLabelClass;
-                            }
-
-                            return errorLabelClasses;
-                        })()
-                    }).insertAfter(selector);
-                }
-            }
-
-            function _clearForm(form) {
-                form[0].reset();
-            }
-
-            function _createModal(status) {
-                var messageHtml;
-
-                if (status === 'fail') {
-                    messageHtml = $('<div/>', {
-                        'class': 'modal',
-                        id: 'errorMessage',
-                        html: 'Ошибка сервера, попробуйте еще раз'
-                    });
-                } else if (status === 'success') {
-                    messageHtml = $('<div/>', {
-                        'class': 'modal',
-                        id: 'successMessage',
-                        html: 'Все круто. Даннын отправились на сервер!'
-                    });
-                }
-
-                messageHtml
-                    .appendTo('body')
-                    .bPopup({
-                        transition: 'slideDown'
-                    });
-            }
-
-            function _sendData() {
-                if (errors.length) {
-                    console.log('You have arrors');
-                } else {
-                    $.ajax({
-                            url: serverUrl,
-                            type: 'POST',
-                            contentType: 'application/json; charset-utf-8',
-                            dataType: 'json',
-                            data: form.serialize()
-                        })
-                        .done(function () {
-                            console.log("success");
-                        })
-                        .fail(_createModal('fail'))
-                        .always(function () {
-                            console.log("complete");
-                        });
-                }
-            }
-        };
-
+    function Validate(element, options){
+        this.settings =  $.extend({}, defaults, options);
+        this.element = element;
         this.init();
+    }
 
-        return this;
+    Validate.prototype.init = function() {
+        console.log('[ Start Validate Module ... ]');
+
+        var form = $(this.element),
+            settings = this.settings,
+            formInputs = form.find('input, textarea'),
+            errors = [],
+            serverUrl = form.attr('data-url');
+
+        //Start module when submitted
+        $(document).on('submit', form, function (e) {
+            e.preventDefault();
+
+            _checkEvery(formInputs);
+            _addListeners(formInputs);
+            _sendData();
+        });
+
+        function _addListeners(selectors) {
+            if (selectors) {
+
+                for (var i = 0; i < selectors.length; i++) {
+                    var currentItem = $(selectors[i]);
+
+                    (function (ci) {
+                        $(document).on('keyup', currentItem, function () {
+                            _checkCurrent(ci);
+                        });
+                    })(currentItem);
+
+                }
+
+            }
+        }
+
+        function _checkCurrent(selector) {
+            var valid = !_isEmpty(selector.val());
+            valid ? _doValid(selector) : _doInvalid(selector);
+        }
+
+        function _checkEvery(selectors) {
+            if (selectors) {
+                errors = [];
+
+                for (var i = 0; i < selectors.length; i++) {
+                    var currentInput = $(selectors[i]);
+                    _checkCurrent(currentInput);
+                }
+            }
+        }
+
+        function _isEmpty(value) {
+            return !Boolean(value);
+        }
+
+        function _doValid(selector) {
+            var itemId = selector.attr('id'),
+                errorLabelId = selector.attr('name') + '-error',
+                index;
+
+            selector.removeClass(settings.errorInputClass);
+            index = $.inArray(itemId, errors);
+            errors.splice(index, 1);
+            $('#' + errorLabelId).remove();
+        }
+
+        function _doInvalid(selector) {
+            var itemId = selector.attr('id'),
+                errorLabelId = selector.attr('name') + '-error',
+                errorLabelLeft = selector.attr('data-error-left'),
+                errorLabelLeftClass = selector.attr('data-error-left-class'),
+                errorLabelMessage = selector.attr('data-empty-error'),
+                errorLabelClasses;
+
+            selector.addClass(settings.errorInputClass);
+            errors.push(itemId);
+
+            if ($('#' + errorLabelId).length === 0) {
+                $('<label>', {
+                    id: errorLabelId,
+                    text: function () {
+                        return errorLabelMessage ? errorLabelMessage : settings.emptyMessage;
+                    },
+                    'for': itemId,
+                    'class': (function () {
+                        if (errorLabelLeft) {
+                            errorLabelClasses = settings.errorLabelClass + ' ' + (errorLabelLeftClass ? errorLabelLeftClass : settings.errorLabelLeftClass);
+                        } else {
+                            errorLabelClasses = settings.errorLabelClass;
+                        }
+
+                        return errorLabelClasses;
+                    })()
+                }).insertAfter(selector);
+            }
+        }
+
+        function _clearForm(form) {
+            form[0].reset();
+        }
+
+        function _createModal(status) {
+            var messageHtml;
+
+            if (status === 'fail') {
+                messageHtml = $('<div/>', {
+                    'class': 'modal',
+                    id: 'errorMessage',
+                    html: 'Ошибка сервера, попробуйте еще раз'
+                });
+            } else if (status === 'success') {
+                messageHtml = $('<div/>', {
+                    'class': 'modal',
+                    id: 'successMessage',
+                    html: 'Все круто. Даннын отправились на сервер!'
+                });
+            }
+
+            messageHtml
+                .appendTo('body')
+                .bPopup({
+                    transition: 'slideDown'
+                });
+        }
+
+        function _sendData() {
+            if (errors.length) {
+                console.log('You have arrors');
+            } else {
+                $.ajax({
+                        url: serverUrl,
+                        type: 'POST',
+                        contentType: 'application/json; charset-utf-8',
+                        dataType: 'json',
+                        data: form.serialize()
+                    })
+                    .done(function () {
+                        console.log("success");
+                    })
+                    .fail(_createModal('fail'))
+                    .always(function () {
+                        console.log("complete");
+                    });
+            }
+        }
+    };
+
+    $.fn.validate = function (options) {
+        new Validate(this, options);
+        return this
     }
 
 }(jQuery));
