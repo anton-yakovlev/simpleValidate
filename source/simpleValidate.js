559d153f7241030f3bff69e41075a2a852e6c270
Anton Yakovlev <yakovlev.business@gmail.com>
2015-12-20 21:04:07 +0200

    text commit v1

--- source/simpleValidate.js
+++ source/simpleValidate.js
@@ -1,166 +1,203 @@
 // jQuery validation plugin by Anton Yakovlev
 
 ;(function ($) {
+    $.extend($.fn, {
+        validate: function (options) {
+            console.log('[ Validate func ...]');
 
-    // -------- Public methods -------- //
-    var defaults = {
-        errorInputClass: 'error', // Class for invalid input
-        errorLabelClass: 'label-error', // Class for generated label, if input is invalid
-        errorLabelLeftClass: 'label-error_left', // Class for generated label with left position, if input is invalid
-        emptyMessage: 'Заполните поле' // Default message in tooltip
-    };
+            // If nothing is selected, return nothing; can't chain anyway
+            if (!this.length) {
+                if (options && options.debug && window.console) {
+                    console.warn("Nothing selected, can't validate, returning nothing.");
+                }
+                return;
+            }
 
+            // Check if a validator for this form was already created
+            var validator = $.data(this[0], "validator");
+            if (validator) {
+                return validator;
+            }
 
-    // -------- Module constructor -------- //
-    function Validator(element, options) {
-        this.settings = $.extend({}, defaults, options);
-        this.element = element;
-        this.init();
-    }
+            // Add novalidate tag if HTML5.
+            this.attr("novalidate", "novalidate");
 
+            validator = new $.validator(this[0], options);
+            $.data(this[0], "validator", validator);
 
-    // -------- Module init -------- //
-    Validator.prototype.init = function () {
-        var formElement = $(this.element),
-            settings = this.settings;
+            this.on('submit', function (event) {
+                event.preventDefault();
 
-        _addGeneralListeners();
-
-        // -------- Listeners -------- //
-        // General listeners
-        function _addGeneralListeners() {
-            $(document).on('submit', formElement, function (e) {
-                console.log('[ Start Validate Module by submit ... ]');
-
-                e.preventDefault();
-
-                // Validate form
-                var valid = _validation(formElement);
-
-                // Add error listeners if inputs are invalid
-                if (!_validation(formElement)) {
-                    _addErrorListeners(formElement);
-                    console.log('[_addErrorListeners ...]');
+                if (validator.form()) {
+                    console.log('validator.form() : true');
+                    return true;
                 } else {
-                    console.log('[return valid] : ' + valid);
-                    _clearForm(formElement);
-                    return valid;
+                    console.log('validator.form() : false');
+                    //validator.focusInvalid();
+                    return false;
                 }
             });
+
+            return validator;
+        },
+        valid: function () {
+
         }
-
-        // Add listeners on every input
-        function _addErrorListeners(form) {
-            var formInputs = form.find('input, textarea').not('input[type="file"], input[type="hidden"]');
-
-            for (var i = 0; i < formInputs.length; i++) {
-                var currentItem = $(formInputs[i]),
-                    currentItemId = '#' + $(formInputs[i]).attr('id');
-
-                (function (ci) {
-                    $(document).on('keyup', currentItemId, function () {
-                        _checkCurrent(ci);
-                    });
-                })(currentItem);
-            }
-        }
-
-        // Remove listeners from every input
-        function _removeErrorListeners(form) {
-            var formInputs = form.find('input, textarea').not('input[type="file"], input[type="hidden"]');
-
-            for (var i = 0; i < formInputs.length; i++) {
-                var currentItemId = '#' + $(formInputs[i]).attr('id');
-                $(document).off('keyup', currentItemId);
-            }
-        }
+    });
 
 
-        // -------- Validate form -------- //
-        // General validation
-        function _validation(form) {
-            var formInputs = form.find('input, textarea').not('input[type="file"], input[type="hidden"]'),
-                valid = true;
-
-            if (!_checkEvery(formInputs)) valid = false;
-
-            return valid;
-        }
-
-        // Check every input for valid data
-        function _checkEvery(selectors) {
-            var valid = true;
-
-            for (var i = 0; i < selectors.length; i++) {
-                if (!_checkCurrent($(selectors[i]))) valid = false;
-            }
-
-            return valid;
-        }
-
-        // Check current input
-        function _checkCurrent(selector) {
-            var valid = !_isEmpty(selector.val());
-            valid ? _doValid(selector) : _doInvalid(selector);
-            return valid;
-        }
-
-        // Validate for empty data
-        function _isEmpty(value) {
-            return !Boolean(value);
-        }
-
-        // Generate valid markup in form
-        function _doValid(selector) {
-            var errorLabelId = selector.attr('name') + '-error';
-
-            selector.removeClass(settings.errorInputClass);
-            $('#' + errorLabelId).remove();
-        }
-
-        // Generate invalid markup in form
-        function _doInvalid(selector) {
-            var itemId = selector.attr('id'),
-                errorLabelId = selector.attr('name') + '-error',
-                $errorLabelId = $('#' + errorLabelId),
-                errorLabelLeft = selector.data('errorLeft'),
-                errorLabelLeftClass = selector.data('errorLeftClass'),
-                errorLabelMessage = selector.data('emptyError');
-
-            selector.addClass(settings.errorInputClass);
-
-            if (!$errorLabelId.length) {
-                $('<label>', {
-                    id: errorLabelId,
-                    text: function () {
-                        return errorLabelMessage ? errorLabelMessage : settings.emptyMessage;
-                    },
-                    'for': itemId,
-                    'class': (function () {
-                        var errorLabelClasses;
-
-                        if (errorLabelLeft) {
-                            errorLabelClasses = settings.errorLabelClass + ' ' + (errorLabelLeftClass ? errorLabelLeftClass : settings.errorLabelLeftClass);
-                        } else {
-                            errorLabelClasses = settings.errorLabelClass;
-                        }
-
-                        return errorLabelClasses;
-                    })()
-                }).insertAfter(selector);
-            }
-        }
-
-        // Clear form and reset validation markup
-        function _clearForm(form) {
-            form[0].reset();
-            _removeErrorListeners(form);
-        }
+    // -------- Validator constructor -------- //
+    $.validator = function (formElement, options) {
+        this.settings = $.extend(true, {}, $.validator.defaults, options);
+        this.currentForm = formElement;
+        this.init();
     };
 
-    $.fn.validate = function (options) {
-        new Validator(this, options);
-        return this
-    }
+    $.extend($.validator, {
+
+        // -------- Default settings -------- //
+        defaults: {
+            errorInputClass: 'error', // Class for invalid input
+            errorLabelClass: 'label-error', // Class for generated label, if input is invalid
+            errorLabelLeftClass: 'label-error_left', // Class for generated label with left position, if input is invalid
+            emptyMessage: 'Заполните поле' // Default message in tooltip
+        },
+
+        setDefaults: function (options) {
+            $.extend($.validator.defaults, options);
+        },
+
+        prototype: {
+            // Init function
+            init: function () {
+                this.submitted = {};
+                this.invalid = {};
+                this.valid = true;
+                this.errorList = [];
+            },
+
+            // Check whole form procedure
+            form: function () {
+                console.log('[ form() function ...]');
+
+                this.checkForm();
+
+                if (!this.valid()) {
+                    // Invalid form
+                    console.log('[ form() function ... addErrorListeners]');
+                    this.addErrorListeners();
+                }
+
+                return this.valid();
+            },
+
+            // Check all inputs
+            checkForm: function () {
+                var selectors = $(this.currentForm).find('input, textarea').not('input[type="file"], input[type="hidden"]');
+
+                for (var i = 0; i < selectors.length; i++) {
+                    this.check( $(selectors[i]) );
+                }
+
+                return this.valid();
+            },
+
+            // Check current input
+            check: function (selector) {
+                var elementValue = selector.val();
+
+                if (this.isEmpty(elementValue)) {
+                    this.errorList.push('error');
+                    return;
+                }
+
+                return true;
+            },
+
+            // Add listeners on every input
+            addErrorListeners: function () {
+                var formInputs = $(this.currentForm).find('input, textarea').not('input[type="file"], input[type="hidden"]'),
+                    checkFn = this.check;
+
+                for (var i = 0; i < formInputs.length; i++) {
+                    var currentItem = $(formInputs[i]),
+                        currentItemId = '#' + $(formInputs[i]).attr('id');
+
+                    (function (ci) {
+                        $(document).on('keyup', currentItemId, function () {
+                            checkFn(ci);
+                        });
+                    })(currentItem);
+                }
+            },
+
+            // Remove listeners from every input
+            removeErrorListeners: function () {
+                var formInputs = $(this.currentForm).find('input, textarea').not('input[type="file"], input[type="hidden"]');
+
+                for (var i = 0; i < formInputs.length; i++) {
+                    var currentItemId = '#' + $(formInputs[i]).attr('id');
+                    $(document).off('keyup', currentItemId);
+                }
+            },
+
+            // Generate valid markup in form
+            doValid: function (selector) {
+                var errorLabelId = selector.attr('name') + '-error';
+
+                selector.removeClass(settings.errorInputClass);
+                $('#' + errorLabelId).remove();
+            },
+
+            // Generate invalid markup in form
+            doInvalid: function (selector) {
+                var itemId = selector.attr('id'),
+                    errorLabelId = selector.attr('name') + '-error',
+                    $errorLabelId = $('#' + errorLabelId),
+                    errorLabelLeft = selector.data('errorLeft'),
+                    errorLabelLeftClass = selector.data('errorLeftClass'),
+                    errorLabelMessage = selector.data('emptyError');
+
+                selector.addClass(this.settings.errorInputClass);
+
+                if (!$errorLabelId.length) {
+                    $('<label>', {
+                        id: errorLabelId,
+                        text: function () {
+                            return errorLabelMessage ? errorLabelMessage : this.settings.emptyMessage;
+                        },
+                        'for': itemId,
+                        'class': (function () {
+                            var errorLabelClasses;
+
+                            if (errorLabelLeft) {
+                                errorLabelClasses = this.settings.errorLabelClass + ' ' + (errorLabelLeftClass ? errorLabelLeftClass : this.settings.errorLabelLeftClass);
+                            } else {
+                                errorLabelClasses = this.settings.errorLabelClass;
+                            }
+
+                            return errorLabelClasses;
+                        })()
+                    }).insertAfter(selector);
+                }
+            },
+
+            // IsValid form
+            valid: function () {
+                return this.size() === 0;
+            },
+
+            // Error list length
+            size: function () {
+                return this.errorList.length;
+            },
+
+            // Validate for empty data
+            isEmpty: function (value) {
+                return !Boolean(value);
+            }
+        }
+
+    });
 
 }(jQuery));
